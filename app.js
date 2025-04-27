3fdb76c08b464ed4deb0b12291995be16ff005e2
Javlon1221 <avazbekzalilov835@gmail.com>
2025-04-27 18:29:25 +0500

    xato va kam chiliklar to'gitlandi

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+// Login Va Parolni tekshirish
+
+// const login_btnEl = document.querySelector(".login_btn")
+// const loginEl = document.querySelector(".login")
+
+// loginEl.addEventListener("submit", (event)=>{
+//     event.preventDefault();
+//     const loginInput = document.querySelector('.user').value;
+//     const passwordInput = document.querySelector('.password').value;
+
+//     const checkLogin = "User123";
+//     const checkPass = "Parol123";
+
+//     if(loginInput === checkLogin && passwordInput === checkPass){
+//         window.location.href = "./html/page.html"
+//     }else{
+//         alert("Login yoki parolda xatolik bor!")
+//     }
+
+// })
+
+// Tablega ma'lumot qo'shish
+
 const create_btnEl = document.getElementById("create_btn");
 const tbodyEl = document.querySelector("tbody");
 const users = [];
@@ -27,4 +50,13 @@ create_btnEl.addEventListener("click", (event) => {
     
     fnameInputEl.value = "";
     lnameInputEl.value = "";
+});
+
+// button bosilganda create_table ochiladi
+
+const toggleCreateTableBtn = document.getElementById("toggle_create_table");
+const createTableDiv = document.querySelector(".create_table");
+
+toggleCreateTableBtn.addEventListener("click", function() {
+    createTableDiv.classList.toggle("show");
 });
